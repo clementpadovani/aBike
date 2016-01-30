e7f3b60e436241ef287abdf0256eca6aa7e72434
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-01-30 16:30:53 +0100

    Created aBike—Nantes.

---
+++ aBike—Nantes/VEAppDelegate.m
@@ -0,0 +1,135 @@
+//
+//  VEAppDelegate.m
+//  aBike—Nantes
+//
+//  Created by Clément Padovani on 8/8/14.
+//  Copyright (c) 2014 Clément Padovani. All rights reserved.
+//
+
+#import "VEAppDelegate.h"
+
+@implementation VEAppDelegate
+
+- (id) init
+{
+	self = [super init];
+	
+	if (self)
+	{
+		[VEConsul sharedConsul];
+		
+		[[VEConsul sharedConsul] setDelegate: self];
+		
+		[[VEConsul sharedConsul] setup];
+	}
+	
+	return self;
+}
+
+- (NSString *) contractNameForConsul:(VEConsul *)consul
+{
+	return @"nantes";
+}
+
+- (NSString *) cityNameForConsul:(VEConsul *)consul
+{
+	return @"Nantes";
+}
+
+- (NSString *) cityServiceNameForConsul:(VEConsul *)consul
+{
+	return @"Bicloo";
+}
+
+- (NSString *) cityRegionNameForConsul: (VEConsul *) consul
+{
+	return NSLocalizedString(@"Nantes", @"");
+}
+
+- (UIColor *) mainColorForConsul: (VEConsul *) consul
+{
+	return [UIColor colorWithRed: (255.f / 255.f) green: (215.f / 255.f) blue: (.0f / 255.f) alpha: 1];
+}
+
+- (MKCoordinateRegion) mapRegionForConsul: (VEConsul *) consul
+{
+	return MKCoordinateRegionMake(CLLocationCoordinate2DMake(47.217763, -1.552311), MKCoordinateSpanMake(0.057086, 0.057086));
+}
+
+#if (SCREENSHOTS==1)
+
+
+- (CLLocation *) locationForScreenshots
+{
+	return [[CLLocation alloc] initWithLatitude: 47.217239 longitude: -1.553412];
+}
+
+#endif
+
+- (UIWindow *) window
+{
+	return (UIWindow *) [[VEConsul sharedConsul] window];
+}
+
+- (BOOL) application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	return [[VEConsul sharedConsul] applicationWillFinishLaunchingWithOptions: launchOptions];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	return [[VEConsul sharedConsul] applicationDidFinishLaunchingWithOptions: launchOptions];
+}
+
+- (BOOL) application: (UIApplication *) application openURL: (NSURL *) url sourceApplication: (NSString *) sourceApplication annotation: (id) annotation
+{
+	return [[VEConsul sharedConsul] applicationOpenURL: url sourceApplication: sourceApplication annotation: annotation];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationWillResignActive];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationDidBecomeActive];
+}
+
+- (void) applicationDidEnterBackground:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationDidEnterBackground];
+}
+
+- (void) applicationWillEnterForeground:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationWillReturnToForeground];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationWillTerminate];
+}
+
+- (void) applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+	[[VEConsul sharedConsul] applicationDidReceiveMemoryWarning];
+}
+
+#ifdef DEBUG
+
+- (void) motionBegan: (UIEventSubtype) motion withEvent: (UIEvent *) event
+{
+	if (motion == UIEventSubtypeMotionShake)
+	{
+		[[VEConsul sharedConsul] motionShake];
+		
+		return;
+	}
+	
+	[super motionBegan: motion withEvent: event];
+}
+
+#endif
+
+@end
