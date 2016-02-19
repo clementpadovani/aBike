2dfca55ef9da8443373c72536cf6f18567848a0a
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-02-19 21:49:05 +0100

    Fixes leak.

--- aBikeFramework/aBikeLibrary/Additions/VEReachability.m
+++ aBikeFramework/aBikeLibrary/Additions/VEReachability.m
@@ -98,7 +98,9 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithName(NULL, [hostname UTF8String]);
     if (ref) 
     {
-        id reachability = [[self alloc] initWithReachabilityRef:ref];
+        id reachability = [[self alloc] initWithReachabilityRef:CFBridgingRetain((__bridge id)ref)];
+
+	    CFRelease(ref);
 
         return reachability;
     }
@@ -111,8 +113,10 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr*)hostAddress);
     if (ref) 
     {
-        id reachability = [[self alloc] initWithReachabilityRef:ref];
-        
+        id reachability = [[self alloc] initWithReachabilityRef:CFBridgingRetain((__bridge id)ref)];
+
+	    CFRelease(ref);
+
         return reachability;
     }
     
