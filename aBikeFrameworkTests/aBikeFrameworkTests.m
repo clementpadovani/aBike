9d7a5f70f82dca6c9a25ae2ec0f0ba1e83233512
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-03-13 01:09:31 +0100

    As-is.

--- aBikeFrameworkTests/aBikeFrameworkTests.m
+++ aBikeFrameworkTests/aBikeFrameworkTests.m
@@ -14,11 +14,22 @@
 
 #import "VEaBikeFrameworkTestsConsulDelegate.h"
 
+#import "VEConstants.h"
+
 @interface aBikeFrameworkTests : XCTestCase
 
 @end
 
 @implementation aBikeFrameworkTests
+
++ (BOOL) isRunningOnCI
+{
+	BOOL isRunningOnCI = [[[NSProcessInfo processInfo] environment][@"CI"] boolValue];
+
+	BOOL isRunningOnTravis = [[[NSProcessInfo processInfo] environment][@"TRAVIS"] boolValue];
+
+	return isRunningOnCI && isRunningOnTravis;
+}
 
 + (void) setUp
 {
@@ -60,7 +71,13 @@
 
 	XCTAssertNotNil(stationsDownloadURL);
 
-	
+	NSString *downloaderAPIToken;
+
+	if (![[self class] isRunningOnCI])
+		downloaderAPIToken = kJCDecauxAPIKey;
+//	else
+
+	JCDECAUXAPITOKEN=2d93849e709748d86c10c303ba94a773f14de3a2
 }
 
 @end
