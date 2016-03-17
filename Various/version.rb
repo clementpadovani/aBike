b35fd98070146f953c04777ac1430e76ffb5436c
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-03-17 19:25:19 +0100

    Update the version number too

--- Various/version.rb
+++ Various/version.rb
@@ -23,3 +23,29 @@ def bumpBuildNumber
     end
 
 end
+
+def updateBuildVersion(version)
+
+	xcconfig_path = File.expand_path File.dirname(__FILE__) + "/CurrentVersion.xcconfig"
+
+	build_settings = Hash[*File.read(xcconfig_path).lines.map{|x| x.split(/\s*=\s*/, 2)}.flatten]
+
+	build_settings["CP_CURRENT_VERSION"] = "#{version}\n"
+
+	File.open(xcconfig_path, "w")
+
+	build_settings.each do |key,value|
+    	File.open(xcconfig_path, "a") {|file| file.puts "#{key} = #{value}"}
+    end
+
+end
+
+def getCurrentBuildVersion
+	xcconfig_path = File.expand_path File.dirname(__FILE__) + "/CurrentVersion.xcconfig"
+
+	build_settings = Hash[*File.read(xcconfig_path).lines.map{|x| x.split(/\s*=\s*/, 2)}.flatten]
+
+	currentVersion = build_settings["CP_CURRENT_VERSION"]
+
+	currentVersion
+end
