b0ad84a16265dfdd5f238c8365914cccd5d81c44
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-03-27 18:56:35 +0200

    Added update script.

--- Vendor/upgrade-xctool.sh
+++ Vendor/upgrade-xctool.sh
@@ -2,5 +2,14 @@
 
 set -e
 
-brew update
-brew outdated xctool || brew upgrade xctool
+XCODEBUILD_VERSION=$(xcodebuild -version)
+XCODEBUILD_VERSION=`expr "$XCODEBUILD_VERSION" : '^.*Build version \(.*\)'`
+
+if [ $XCODEBUILD_VERSION == "7D175" ]
+then
+	echo "Is Xcode 7.3"
+	brew update
+	brew uninstall xctool && brew install --HEAD xctool
+else
+	echo "Isn't Xcode 7.3"
+fi
