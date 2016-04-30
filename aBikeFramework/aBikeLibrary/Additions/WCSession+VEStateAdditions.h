f011f4a1b71f8c6c7434b4320b2123d642e238c1
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-04-30 18:29:06 +0200

    Created WCSession additions.

---
+++ aBikeFramework/aBikeLibrary/Additions/WCSession+VEStateAdditions.h
@@ -0,0 +1,13 @@
+//
+//  WCSession+VEStateAdditions.h
+//  aBike—Lyon
+//
+//  Created by Clément Padovani on 4/30/16.
+//  Copyright © 2016 Clement Padovani. All rights reserved.
+//
+
+#import <WatchConnectivity/WatchConnectivity.h>
+
+@interface WCSession (VEStateAdditions)
+
+@end
---
+++ aBikeFramework/aBikeLibrary/Additions/WCSession+VEStateAdditions.m
@@ -0,0 +1,13 @@
+//
+//  WCSession+VEStateAdditions.m
+//  aBike—Lyon
+//
+//  Created by Clément Padovani on 4/30/16.
+//  Copyright © 2016 Clement Padovani. All rights reserved.
+//
+
+#import "WCSession+VEStateAdditions.h"
+
+@implementation WCSession (VEStateAdditions)
+
+@end
