00a73deaec1f18dd9e72bbf25a23f5b7da975aef
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-06-24 23:24:37 +0200

    Added update metadata method.

--- fastlane/aBike.rb
+++ fastlane/aBike.rb
@@ -6,28 +6,30 @@ module CPaBike
 
 VERSION = '3.1.0'
 
-# RELEASE_NOTES = ({
-# 
-# 	'en-US' => "Bug fixes",
-# 	'fr-FR' => "Corrections de bugs",
-# 	'en-GB' => "Bug fixes"
-# 
-# })
-
 RELEASE_NOTES = ({
 
-	'en-US' => "- Added Apple Watch app",
-	'fr-FR' => "- Ajout d’une app Apple Watch",
-	'en-GB' => "- Added Apple Watch app"
+  'en-US' => "Bug fixes",
+  'fr-FR' => "Corrections de bugs",
+  'en-GB' => "Bug fixes"
 
 })
+
+# RELEASE_NOTES = ({
+#
+#   'en-US' => "- Added Apple Watch app",
+#   'fr-FR' => "- Ajout d’une app Apple Watch",
+#   'en-GB' => "- Added Apple Watch app"
+#
+# })
 
 SUBMIT_FOR_REVIEW = false
 
 AUTOMATIC_RELEASE = true
 
 SUBMISSION_INFORMATION = ({
-	add_id_info_uses_idfa: false
+	add_id_info_uses_idfa: false,
+  demo_user: "",
+  demo_password: ""
 })
 
 end
