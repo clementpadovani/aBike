ee53570262781734ed949d0c876e591fa3660fb1
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-09-10 16:58:40 +0200

    As-is.

---
+++ aBikeFramework/aBikeLibrary/Additions/NSUserActivity+VEAdditions.h
@@ -0,0 +1,19 @@
+//
+//  NSUserActivity+VEAdditions.h
+//  aBike—Lyon
+//
+//  Created by Clément Padovani on 9/10/16.
+//  Copyright © 2016 Clement Padovani. All rights reserved.
+//
+
+@import Foundation;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSUserActivity (VEAdditions)
+
+//+ (instancetype)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ aBikeFramework/aBikeLibrary/Additions/NSUserActivity+VEAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSUserActivity+VEAdditions.m
+//  aBike—Lyon
+//
+//  Created by Clément Padovani on 9/10/16.
+//  Copyright © 2016 Clement Padovani. All rights reserved.
+//
+
+#import "NSUserActivity+VEAdditions.h"
+
+@implementation NSUserActivity (VEAdditions)
+
+@end
