736476015e2cc7796742f8235fffa81c7141c01c
Clément Padovani <ClementPadovani@users.noreply.github.com>
2017-11-04 00:49:28 +0100

    Updated pods

--- Pods/Target Support Files/SimulatorStatusMagic/SimulatorStatusMagic-umbrella.h
+++ Pods/Target Support Files/SimulatorStatusMagic/SimulatorStatusMagic-umbrella.h
@@ -1,10 +1,20 @@
 #ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
 #endif
 
 #import "SDStatusBarManager.h"
 #import "SDStatusBarOverrider.h"
 #import "SDStatusBarOverriderPost10_0.h"
+#import "SDStatusBarOverriderPost10_3.h"
+#import "SDStatusBarOverriderPost11_0.h"
 #import "SDStatusBarOverriderPost8_3.h"
 #import "SDStatusBarOverriderPost9_0.h"
 #import "SDStatusBarOverriderPost9_3.h"
